aa4f426f86e0d3af9b319d94b52f723ef228d5c4
N00N <up201807530@fe.up.pt>
2024-10-04 18:57:14 +0100

    Updates to web scrapping

--- Web scrapping/WebScrappingWikiSpecies.py
+++ Web scrapping/WebScrappingWikiSpecies.py
@@ -1,26 +1,32 @@
+from lxml import html
 import requests
-from bs4 import BeautifulSoup
 
-r = requests.get('https://species.wikimedia.org/w/index.php?title=Special:AllPages&from=A+History+of+British+Ferns')
+# Text file because json is only for the data according to the wikipedia
+file = open("WikiSpecies.txt","w")
+nextPage = True
+url = 'https://species.wikimedia.org/w/index.php?title=Special:AllPages&from=A+History+of+British+Ferns'
 
-# Prints sucess code
-print(r)
+while(nextPage):
+    response = requests.get(url)
 
-# Parsing the HTML
-soup = BeautifulSoup(r.content, 'html.parser')
+    tree = html.fromstring(response.content)
+    link_titles = tree.xpath('//ul[@class="mw-allpages-chunk"]//li/a/@title')
 
-entriesChunk = soup.find('ul', class_='mw-allpages-chunk')
+    next_page_link = tree.xpath('//a[contains(text(),"Next page")]/@href')
 
-for element in entriesChunk:
-    print(element)
+    # if next_page_link:
+    next_page_url = 'https://species.wikimedia.org' + next_page_link[0]
 
+    for title in link_titles:
+        file.write(title)
+        file.write("\n")
+        print(title)
 
+    print("ola")
+    print(next_page_url)
+    
+    # Changes url if next page exists, otherwise, changes the var to false
+    nextPage = False
 
-#Entry example:
-# 
-#  <li><a href="/wiki/Abacetus_bequaerti" title="Abacetus bequaerti">Abacetus bequaerti</a></li>
-
+file.close()
 # Need to find the next element in the end of the page, that allows me to create a new URL
-
-
-
