30d5e17c98731afdc4cc30c279eaa0e783b2f209
N00N <up201807530@fe.up.pt>
2024-10-06 10:22:27 +0100

    Refactor

---
+++ Web scrapping/WebScrappingWikiPedia.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+
+
+def get_wikipedia_pages():
+   
+    wikipedia_base_url = 'https://en.wikipedia.org/wiki/'
+    fileR = open("wikiSpeciesFilterSpeciesV0.txt", "r")
+
+    species_data = {}
+    
+        
+    for line in fileR:
+           
+            wikipedia_url = wikipedia_base_url + line.strip()  
+            
+        
+            response = requests.get(wikipedia_url)
+            
+            if response.status_code == 200:
+                print(f"Página encontrada: {wikipedia_url}")
+                soup = BeautifulSoup(response.content, 'html.parser')
+                paragraphs = soup.find_all('p')
+                paragraph_texts = [p.get_text().strip() for p in paragraphs if p.get_text().strip()]
+                page_content = '\n\n'.join(paragraph_texts)
+
+                infobox = soup.find('table', {'class': 'infobox'})
+
+                if infobox:
+                
+                    image_tags = infobox.find_all('img')
+                
+                    if len(image_tags) > 0:
+                    
+                        image_url = "https:" + image_tags[0]['src']
+                        print(f"Segunda imagem encontrada: {image_url}")
+                    else:
+                        image_url = "No second image found in infobox"
+                else:
+                    image_url = "No infobox found"
+                             
+                    
+                species_data[line.strip()] = {
+                    "content": page_content,
+                    "image_url": image_url
+                }
+                print(f"Conteúdo da página: {page_content[:100]}")
+            else:
+                print(f"Falha ao obter a página: {wikipedia_url} (Status Code: {response.status_code})")
+
+    with open("species_data.json", "w", encoding="utf-8") as json_file:
+        json.dump(species_data, json_file, ensure_ascii=False, indent=4)
+
+get_wikipedia_pages()
+
--- Web scrapping/WebScrappingWikiSpecies.py
+++ Web scrapping/WebScrappingWikiSpecies.py
@@ -1,7 +1,7 @@
 from lxml import html
-from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Lock
 import requests
-import json
 
 def read_wikiSpecies():
     # Text file because json is only for the data according to the wikipedia
@@ -101,57 +101,4 @@ def filter_wikiSpecies_gpt():
 
 #filter_wikiSpecies()
 
-#filter_wikiSpecies_gpt()
-
-
-def get_wikipedia_pages():
-   
-    wikipedia_base_url = 'https://en.wikipedia.org/wiki/'
-    fileR = open("WikiSpeciesUnderScore.txt", "r")
-
-    species_data = {}
-    
-        
-    for line in fileR:
-           
-            wikipedia_url = wikipedia_base_url + line.strip()  
-            
-        
-            response = requests.get(wikipedia_url)
-            
-            if response.status_code == 200:
-                print(f"Página encontrada: {wikipedia_url}")
-                soup = BeautifulSoup(response.content, 'html.parser')
-                paragraphs = soup.find_all('p')
-                paragraph_texts = [p.get_text().strip() for p in paragraphs if p.get_text().strip()]
-                page_content = '\n\n'.join(paragraph_texts)
-
-                infobox = soup.find('table', {'class': 'infobox'})
-
-                if infobox:
-                
-                    image_tags = infobox.find_all('img')
-                
-                    if len(image_tags) > 0:
-                    
-                        image_url = "https:" + image_tags[0]['src']
-                        print(f"Segunda imagem encontrada: {image_url}")
-                    else:
-                        image_url = "No second image found in infobox"
-                else:
-                    image_url = "No infobox found"
-                             
-                    
-                species_data[line.strip()] = {
-                    "content": page_content,
-                    "image_url": image_url
-                }
-                print(f"Conteúdo da página: {page_content[:100]}")
-            else:
-                print(f"Falha ao obter a página: {wikipedia_url} (Status Code: {response.status_code})")
-
-    with open("species_data.json", "w", encoding="utf-8") as json_file:
-        json.dump(species_data, json_file, ensure_ascii=False, indent=4)
-
-get_wikipedia_pages()
-
+filter_wikiSpecies_gpt()
