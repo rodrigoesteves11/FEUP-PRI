c87e89f636ad6cfc92078556ba9b05bb82907bfa
N00N <up201807530@fe.up.pt>
2024-10-10 15:53:43 +0100

    Added files

--- Web-scrapping/dataSpecification.py
+++ Web-scrapping/dataSpecification.py
@@ -2,7 +2,7 @@ import json
 
 file_paths = [
     'JsonParts/Species_data1.json', 'JsonParts/Species_data2.json', 'JsonParts/Species_data3.json', 
-    'JsonParts/Species_data4.json', 'JsonParts/Species_data5.json'
+    'JsonParts/Species_data4.json', 'JsonParts/Species_data5.json', 'JsonParts/Species_data6.json'
 ]
 
 
@@ -17,24 +17,39 @@ for fileJ in file_paths:
 sections_null = 0
 entries = 0
 goodIntroduction = 0 #Bigger than 300 chars
-
-#for entry in json_global.values():
-#    print(entry.get('introduction'))
-#    print("\n")
-#    print(len(entry.get('introduction')))
-#    break
-
+onlySmallIntro = 0
+totalIntroChar = 0
+totalIntroCharNull = 0
+totalIntroCharNotNull = 0
+sections_notNull = 0
+taxonavigation_null = 0
 
 for entry in json_global.values():
     entries +=1
+    totalIntroChar += len(entry.get('introduction'))
     if (entry.get('sections') == {}):
         sections_null +=1
+        totalIntroCharNull += len(entry.get('introduction'))
+    if(entry.get('sections') != {}):
+        sections_notNull += 1
+        totalIntroCharNotNull += len(entry.get('introduction'))
     if (len(entry.get('introduction')) > 300):
         goodIntroduction += 1
+    if ((entry.get('sections') == {}) &(len(entry.get('introduction')) > 600)):
+        print(entry.get('introduction'))
+        onlySmallIntro += 1
+    if((entry.get('scientific_classification') == {}) & (entry.get('sections') != {})): # DROP THESE VALUES, Miliarium example 
+        #print(entry.get('introduction'))
+        taxonavigation_null += 1
 
 print(f"Number of entries: {entries}\n")
 print(f"Number of entries with null sections: {sections_null}\n")
 print(f"Number of entries introductions bigger than 300 chars: {goodIntroduction}\n")
+print(f"Number of entries with null sections and small introductions: {onlySmallIntro}\n")
+print(f"Average char length in the introduction: {round(totalIntroChar/entries,2)}\n")
+print(f"Number of entries with null taxonavigation: {taxonavigation_null}\n")
+print(f"Average char length in the introduction when sections are null: {round(totalIntroCharNull/sections_null,2)}\n")
+print(f"Average char length in the introduction when sections are not null: {round(totalIntroCharNotNull/sections_notNull,2)}\n")
 
 
 
